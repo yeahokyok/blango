90a454e187a3c768c79e54b1aa63b3ecce6e9206
Teerapat Cintornvich <teerapat.yok@gmail.com>
2023-04-03 05:02:45 +0000

    Finish caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,3 +1,4 @@
+import logging
 from django import template
 from django.contrib.auth import get_user_model
 from django.utils.html import format_html
@@ -7,11 +8,13 @@ from blog.models import Post
 
 user_model = get_user_model()
 register = template.Library()
+logger = logging.getLogger(__name__)
 
 
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
     posts = Post.objects.exclude(pk=post.pk)[:5]
+    logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
     return {"title": "Recent Posts", "posts": posts}
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 import logging
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+
 from blog.models import Post
 from blog.forms import CommentForm
 
