c1939c318f6a99d315a5f99c9d4de19122c29216
Teerapat Cintornvich <teerapat.yok@gmail.com>
2023-04-03 15:49:40 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -46,13 +46,26 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
         'crispy_forms',
         'crispy_bootstrap5',
         "debug_toolbar",
         'blango_auth',
-        'blog'
+        'blog',
+        "allauth",
+        "allauth.account",
+        "allauth.socialaccount",
+        "allauth.socialaccount.providers.google"
     ]
+
+    SITE_ID = 1
+    
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
+
     # You should include the Debug Toolbar middleware as early as possible in the list
     MIDDLEWARE = [
         "debug_toolbar.middleware.DebugToolbarMiddleware",
--- blango/urls.py
+++ blango/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
     ),
     path("accounts/", include("django_registration.backends.activation.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("allauth.urls")),
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
 
 ]
