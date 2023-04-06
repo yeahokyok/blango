30a64d5bd3ba72079132d89104f70184e633bf63
Teerapat Cintornvich <982466+yeahokyok@users.noreply.github.com>
2023-04-06 08:10:47 +0000

    Finish throttling

---
+++ blog/api/throttling.py
@@ -0,0 +1,23 @@
+import random
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
+
+
+class RandomRateThrottle(throttling.BaseThrottle):
+    def allow_request(self, request, view):
+        return random.randint(1, 10) != 1
+
+
+class AnonSustainedThrottle(AnonRateThrottle):
+    scope = "anon_sustained"
+
+
+class AnonBurstThrottle(AnonRateThrottle):
+    scope = "anon_burst"
+
+
+class UserSustainedThrottle(UserRateThrottle):
+    scope = "user_sustained"
+
+
+class UserBurstThrottle(UserRateThrottle):
+    scope = "user_burst"
