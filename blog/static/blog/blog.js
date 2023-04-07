2b6eb64879b737d3b83ceef7fcca8d0b5aa4b084
Teerapat Cintornvich <982466+yeahokyok@users.noreply.github.com>
2023-04-07 07:44:10 +0000

    Finish JavaScript classes

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,27 +1,24 @@
-for(let i = 0; i < 10; i += 1) {
-  console.log('for loop i: ' + i)
+class DelayedGreeter extends Greeter {
+  delay = 2000
+
+  constructor (name, delay) {
+    super(name)
+    if (delay !== undefined) {
+      this.delay = delay
+    }
+  }
+
+  greet () {
+    setTimeout(
+      () => {
+        this.showGreeting(this.getGreeting())
+      }, this.delay
+    )
+  }
 }
 
-let j = 0
-while(j < 10) {
-  console.log('while loop j: ' + j)
-  j += 1
-}
+const dg2 = new DelayedGreeter('Patchy 2 Seconds')
+dg2.greet()
 
-let k = 10
-
-do {
-  console.log('do while k: ' + k)
-} while(k < 10)
-
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-numbers.forEach((value => {
-  console.log('For each value ' + value)
-}))
-
-const doubled = numbers.map(value => value * 2)
-
-console.log('Here are the doubled numbers')
-
-console.log(doubled)
+const dg1 = new DelayedGreeter('Patchy 1 Second', 1000)
+dg1.greet()
