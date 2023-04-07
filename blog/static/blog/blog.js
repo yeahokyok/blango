272c603453bd7ca6abe622f4a7cdc7a726dfbfd9
Teerapat Cintornvich <982466+yeahokyok@users.noreply.github.com>
2023-04-07 07:59:03 +0000

    Finish JavaScript promises

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,24 +1,25 @@
-class DelayedGreeter extends Greeter {
-  delay = 2000
+function resolvedCallback(data) {
+  console.log('Resolved with data ' +  data)
+}
 
-  constructor (name, delay) {
-    super(name)
-    if (delay !== undefined) {
-      this.delay = delay
+function rejectedCallback(message) {
+  console.log('Rejected with message ' + message)
+}
+
+const lazyAdd = function (a, b) {
+  const doAdd = (resolve, reject) => {
+    if (typeof a !== "number" || typeof b !== "number") {
+      reject("a and b must both be numbers")
+    } else {
+      const sum = a + b
+      resolve(sum)
     }
   }
 
-  greet () {
-    setTimeout(
-      () => {
-        this.showGreeting(this.getGreeting())
-      }, this.delay
-    )
-  }
+  return new Promise(doAdd)
 }
 
-const dg2 = new DelayedGreeter('Patchy 2 Seconds')
-dg2.greet()
+const p = lazyAdd(3, 4)
+p.then(resolvedCallback, rejectedCallback)
 
-const dg1 = new DelayedGreeter('Patchy 1 Second', 1000)
-dg1.greet()
+lazyAdd("nan", "alsonan").then(resolvedCallback, rejectedCallback)
